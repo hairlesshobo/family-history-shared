fd651ae3f22799d5eff4e2b8fc17fc16b47b1348
Steven Cross <flip@den.foxhollow.cc>
2020-05-09 15:49:06 -0400

    More refactoring

---
+++ Classes/TapeSource.cs
@@ -0,0 +1,11 @@
+namespace Archiver.Classes
+{
+    public class TapeSource
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string[] SourcePaths { get; set; }
+        public string[] ExcludePaths { get; set; }
+        public string[] ExcludeFiles { get; set; }
+    }
+}
