80ad3d76621331ec219652d0acc68bdb28728698
Steve Cross <flip@foxhollow.cc>
2022-12-19 15:22:29 -0500

    Finished populating mediainfo

--- Utilities/MediainfoUtils.cs
+++ Utilities/MediainfoUtils.cs
@@ -3,7 +3,9 @@ using System.Diagnostics;
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Threading;
 using System.Threading.Tasks;
+using FoxHollow.FHM.Shared.Classes;
 using FoxHollow.FHM.Shared.Models;
 using FoxHollow.FHM.Shared.Models.Video;
 
@@ -37,7 +39,7 @@ namespace FoxHollow.FHM.Shared.Utilities
             // Sidecar exists and we are instructed to use it
             if (useSidecar && !refreshSidecar && File.Exists(sidecarPath))
             {
-                Console.WriteLine("Using existing sidecar");
+                Console.WriteLine("Using existing raw mediainfo sidecar");
 
                 using (var sidecarFileHandle = File.Open(sidecarPath, FileMode.Open, FileAccess.Read))
                     return await JsonSerializer.DeserializeAsync<JsonNode>(sidecarFileHandle);
@@ -59,7 +61,7 @@ namespace FoxHollow.FHM.Shared.Utilities
 
                     if (useSidecar)
                     {
-                        Console.WriteLine("Writing mediainfo sidecar");
+                        Console.WriteLine("Writing new raw mediainfo sidecar");
                         await File.WriteAllTextAsync(sidecarPath, mediainfo.ToJsonString(Static.DefaultJso));
                     }
 
@@ -70,8 +72,18 @@ namespace FoxHollow.FHM.Shared.Utilities
             }
         }
 
-        public static async Task<RawSidecar> GenerateRawSidecarAsync(string filePath)
+        /// <summary>
+        ///     Generates the model that is to be stored in the sidecar file
+        ///     of any raw family video
+        /// </summary>
+        /// <param name="filePath">Full path to the file</param>
+        /// <param name="ctk">Optional cancellation token used to cancel the in-progress generation</param>
+        /// <returns>Model used to generate raw sidecar file</returns>
+        public static async Task<RawSidecar> GenerateRawSidecarAsync(string filePath, CancellationToken ctk = default)
         {
+            if (ctk == default(CancellationToken))
+                ctk = CancellationToken.None;
+
             if (!File.Exists(filePath))
                 throw new FileNotFoundException(filePath);
 
@@ -89,42 +101,80 @@ namespace FoxHollow.FHM.Shared.Utilities
             JsonObject videoTrack = MediainfoUtils.FindTrack((JsonArray)tracks, "video");
             JsonObject audioTrack = MediainfoUtils.FindTrack((JsonArray)tracks, "audio");
 
+            var hash = new RawSidecarHash();
+            
+            using (var fileStream = File.Open(fileInfo.FullName, FileMode.Open, FileAccess.Read))
+            {
+                var hashGenerator = new HashStreamGenerator(fileStream);
+                hashGenerator.GenerateSha1 = true;
+
+                var hashes = await hashGenerator.GenerateAsync(ctk);
+
+                hash.MD5 = hashes.Md5Hash;
+                hash.SHA1 = hashes.Sha1Hash;
+            }
+
+            var general = new RawSidecarGeneral()
+            {
+                FileName = fileInfo.Name,
+                ContainerFormat = generalTrack["Format"].GetValue<string>(),
+                Size = fileInfo.Length,
+                Duration = TimeSpan.FromSeconds(Double.Parse(generalTrack["Duration"].GetValue<string>()))
+            };
+
+            var encodedDate = generalTrack["Encoded_Date"]?.GetValue<string>();
+            if (!String.IsNullOrWhiteSpace(encodedDate))
+                general.CaptureDtm = DateTime.ParseExact(encodedDate, "UTC yyyy-MM-dd HH:mm:ss", null);
+
+            var video = new RawSidecarVideo()
+            {
+                VideoWidth = uint.Parse(videoTrack["Width"].GetValue<string>()),
+                VideoHeight = uint.Parse(videoTrack["Height"].GetValue<string>()),
+                Format = GetVideoFormat(videoTrack, videoTrack["Format"].GetValue<string>()),
+                FormatName = videoTrack["Format_Commercial_IfAny"]?.GetValue<string>(),
+                BitrateMode = GetBitrateMode(videoTrack["Bitrate_Mode"]?.GetValue<string>()),
+                Bitrate = uint.Parse(videoTrack["BitRate"].GetValue<string>()),
+                FramerateMode = GetFramerateMode(videoTrack["FrameRate_Mode"]?.GetValue<string>()), // TODO: It appears that mpeg2 can be assumed to be constant
+                FrameRate = Double.Parse(videoTrack["FrameRate"].GetValue<string>()),
+                FrameCount = ulong.Parse(videoTrack["FrameCount"].GetValue<string>()),
+                ScanType = GetScanType(videoTrack["ScanType"]?.GetValue<string>())
+            };
+            
+            var audio = new RawSidecarAudio()
+            {
+                Format = audioTrack["Format"].GetValue<string>(),
+                BitrateMode = GetBitrateMode(audioTrack["BitRate_Mode"].GetValue<string>()),
+                Bitrate = uint.Parse(audioTrack["BitRate"].GetValue<string>()),
+                SamplingRate = uint.Parse(audioTrack["SamplingRate"].GetValue<string>()),
+                Channels = byte.Parse(audioTrack["Channels"].GetValue<string>()),
+                BitDepth = byte.Parse(audioTrack["BitDepth"]?.GetValue<string>() ?? "16")
+            };
+
             // build sidecar model
             var sidecar = new RawSidecar()
             {
-                General = new RawSidecarGeneral()
-                {
-                    FileName = fileInfo.Name,
-                    ContainerFormat = generalTrack["Format"].GetValue<string>(),
-                    Size = fileInfo.Length,
-                    CaptureDtm = DateTime.ParseExact(generalTrack["Encoded_Date"].GetValue<string>(), "UTC yyyy-MM-dd HH:mm:ss", null),
-                    Duration = TimeSpan.FromSeconds(Double.Parse(generalTrack["Duration"].GetValue<string>()))
-                },
-                Video = new RawSidecarVideo()
-                {
-                    VideoWidth = uint.Parse(videoTrack["Width"].GetValue<string>()),
-                    VideoHeight = uint.Parse(videoTrack["Height"].GetValue<string>()),
-                    Format = videoTrack["Format"].GetValue<string>(),
-                    FormatName = videoTrack["CodecID"]?.GetValue<string>(),
-                    BitrateMode = GetBitrateMode(videoTrack["Bitrate_Mode"]?.GetValue<string>()),
-                    Bitrate = uint.Parse(videoTrack["BitRate"].GetValue<string>()),
-                    FramerateMode = GetFramerateMode(videoTrack["FrameRate_Mode"].GetValue<string>()),
-                    FrameRate = Double.Parse(videoTrack["FrameRate"].GetValue<string>()),
-                    FrameCount = ulong.Parse(videoTrack["FrameCount"].GetValue<string>()),
-                    ScanType = GetScanType(videoTrack["ScanType"].GetValue<string>())
-                },
-                Audio = new RawSidecarAudio()
-                {
-                    Format = audioTrack["Format"].GetValue<string>(),
-                    BitrateMode = GetBitrateMode(audioTrack["BitRate_Mode"].GetValue<string>()),
-                    Bitrate = uint.Parse(audioTrack["BitRate"].GetValue<string>()),
-                    SamplingRate = uint.Parse(audioTrack["SamplingRate"].GetValue<string>()),
-                    Channels = byte.Parse(audioTrack["Channels"].GetValue<string>()),
-                    BitDepth = byte.Parse(audioTrack["BitDepth"]?.GetValue<string>() ?? "16")
-                }
+                General = general,
+                Video = video,
+                Audio = audio,
+                Hash = hash
             };
 
             return sidecar;
+        }
+
+        private static bool TrackParamEquals(JsonNode node, string strCompare)
+            => String.Equals(node.GetValue<string>(), strCompare, StringComparison.InvariantCultureIgnoreCase);
+
+        // TODO: create GetAudioFormat method too
+
+        private static string GetVideoFormat(JsonObject videoTrack, string fallback)
+        {
+            // TODO: add translation here for mpeg2, h.264, etc
+
+            if (TrackParamEquals(videoTrack["Format"], "MPEG Video") && TrackParamEquals(videoTrack["Format_Version"], "2"))
+                return "mpeg2";
+
+            return null;
         }
 
         private static BitrateMode GetBitrateMode(string input)
